075b4dfe503c70ad2b5683f715b685836ee43a75
vockagolordy <mari.mikheeva.06@gmail.com>
2026-05-05 20:57:59 +0300

    Initial commit

---
+++ src/test/java/org/example/cloudapp/CloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cloudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
